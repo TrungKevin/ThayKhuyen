8d6ec50b698fc11abf8006fb4073f498ebe15cef
TrungKevin <nguyentrungkien24sg@gmail.com>
2026-03-23 14:38:30 +0700

    update

---
+++ nguyentrungkien/src/main/java/com/example/nguyentrungkien/NguyentrungkienApplication.java
@@ -0,0 +1,12 @@
+
+package com.example.nguyentrungkien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NguyentrungkienApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NguyentrungkienApplication.class, args);
+    }
+}
